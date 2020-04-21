99b619e91dff91371e6da3b789b83c6dc06e44a9
Austin Lin <21chydra@gmail.com>
2020-04-21 10:58:04 +1000

    Implemented multiline display

--- Search/binary.py
+++ Search/binary.py
@@ -8,7 +8,9 @@ import random
 
 class Array:
     def __init__(self, *args):
-        self.elements = args
+        self.elements = sorted(args)
+        self.length = len(args)
+        self.newRows = [i for i in range(10, self.length, 10)]
         self.highlighted = set({})
         self.result = -1
 
@@ -16,33 +18,25 @@ class Array:
         if array is None:
             array = self.elements
 
+        if len(array) == 0:
+            return -1
+
         for i, n in enumerate(array):
             self.highlighted.add(i + start)
 
         time.sleep(0.5)
 
-        if len(array) == 1:
-            if element == array[0]:
-                return 0 + start
-            else:
-                return -1
-
         middle = len(array) // 2
-
-        left = array[:middle]
-        right = array[middle:]
 
         self.highlighted.clear()
 
-        result = self.search(element, left, 0 + start)
-        if result != -1:
-            self.result = result
-            return result
-
-        result = self.search(element, right, middle + start)
-        if result != -1:
-            self.result = result
-            return result
+        if element == array[middle]:
+            self.result = middle + start
+            return middle
+        elif element < array[middle]:
+            return self.search(element, array[:middle], 0 + start)
+        elif element > array[middle]:
+            return self.search(element, array[middle + 1:], middle + start + 1)
 
         self.highlighted.clear()
         return -1
@@ -55,13 +49,17 @@ class Array:
         font = pygame.font.SysFont('calibri', 30)
 
         x = 5
-        y = 200
+        y = 100
         for i, element in enumerate(self.elements):
             colour = (0, 0, 0)
             if i in self.highlighted:
                 colour = (0, 255, 0)
             if i == self.result:
                 colour = (0, 0, 255)
+
+            if i in self.newRows:
+                y += 150
+                x = 5
 
             text = str(element)
             text = font.render(text, True, (255, 255, 255))
@@ -76,12 +74,12 @@ class Array:
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 
-win = pygame.display.set_mode((1100, 600))
+win = pygame.display.set_mode((1100, 800))
 pygame.display.set_caption('Binary Search')
 
-numbers = Array(*(random.randint(0, 100) for _ in range(10)))
+numbers = Array(*(random.randint(0, 100) for _ in range(40)))
 
-search = threading.Thread(target=numbers.search, args=(numbers.elements[3],))
+search = threading.Thread(target=numbers.search, args=(34,))
 search.start()
 
 run = True
